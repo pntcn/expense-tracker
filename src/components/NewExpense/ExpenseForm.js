7eb55c06a53d6606140a81327a302a524e8adb25
Nemanja Pantic <pantic2022@gmail.com>
2023-03-31 20:15:47 +0200

    add expense, filter & chart

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -44,7 +44,7 @@ const ExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
@@ -80,13 +80,14 @@ const ExpenseForm = (props) => {
           <input
             type='date'
             min='2019-01-01'
-            max='2022-12-31'
+            max='2024-12-31'
             value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
       </div>
       <div className='new-expense__actions'>
+        <button type="button" onClick={props.onCancel}>Cancel</button>
         <button type='submit'>Add Expense</button>
       </div>
     </form>
